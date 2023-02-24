2881a85a4425f32c085cb846af68b55c9223e79e
鍾貽丞 <yicheng@zhongyichengdeMacBook-Air.local>
2023-02-25 01:39:14 +0800

    [feat] First ECA search app commit

---
+++ src/Search/ResultCard/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const tranferPrice = (price) => {
+  const splitNumber = String(price).split('')
+  splitNumber.splice(splitNumber.length - 2, 0, ',').join('')
+  return splitNumber
+}
+
+const ResultCard = ({ node }) => {
+  const {
+    product_name,
+    price,
+    price_discount: discount,
+    product_description,
+  } = node
+
+  const description = product_description[0]?.children[0]?.text ?? ''
+
+  return (
+    <li className={styles['result-card']}>
+      <span className={styles['detail__name']}>
+        {product_name}
+        <div className={styles['detail__tooltip']}>
+          <span className={styles['detail__tooltip__text']}>{description}</span>
+        </div>
+      </span>
+
+      <div className={styles['detail__price']}>
+        <span
+          className={styles['detail__price__original']}
+          style={discount ? { textDecoration: 'line-through' } : { fontSize: 'unset' }}
+        >
+          € {tranferPrice(price)}
+        </span>
+
+        {discount && (
+          <span className={styles['detail__price__discount']}>
+            € {tranferPrice(discount)}
+          </span>
+        )}
+      </div>
+    </li>
+  )
+}
+
+export default ResultCard
---
+++ src/Search/index.js
@@ -0,0 +1,92 @@
+import React, { useState, useCallback, useRef } from 'react';
+import algoliasearch from 'algoliasearch';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import ResultCard from './ResultCard';
+
+import styles from './index.module.scss'
+
+const client = algoliasearch(
+  '8AMGCLA2YS',
+  '18a62be1c6d697316c82bc3d81fdaee2'
+);
+const index = client.initIndex('dev_eca_searchbar');
+
+const Search = () => {
+  const [query, setQuery] = useState('');
+  const [hits, setHits] = useState([]);
+  const focusRef = useRef();
+
+
+  const search = useCallback((event) => {
+    const query = event.target.value;
+    setQuery(query);
+
+    if (query.length >= 3) {
+      index.search(query).then(({ hits }) => {
+        setHits(hits);
+      });
+    } else {
+      setHits([]);
+    }
+  }, []);
+
+  const handleSearchClick = useCallback(() => {
+    focusRef.current.focus()
+  }, [])
+
+  const clearQuery = useCallback(() => {
+    setQuery('')
+    setHits([])
+  }, [])
+
+  return (
+    <div className={styles.container} onClick={() => setHits([])}>
+      <div className={styles['search']}>
+        <div className={styles['search__searchbox']}>
+          <form className={styles['search__searchbox__form']}>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className={styles['search__searchbox__button--search']}
+              onClick={handleSearchClick}
+            />
+            <input
+              type="text"
+              className={styles['search__searchbox__input']}
+              placeholder='search'
+              value={query}
+              ref={focusRef}
+              onChange={search}
+            />
+            {query.length > 0 && (
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                className={styles['search__searchbox__button--cancel']}
+                onClick={clearQuery}
+              />
+            )}
+          </form>
+        </div>
+
+        {hits.length > 0 ? (
+          <ul className={styles['search__results']}>
+            <span className={styles['search__results__title']}>Produkt</span>
+            {hits.map(({ node, objectID }) => {
+              return (
+                <ResultCard key={objectID} node={node} />
+              )
+            }
+            )}
+          </ul>
+        ) : query.length >= 3 && (
+          <span className={styles['search__results__title--error']}>
+            Keine Suchergebnisse.
+          </span>
+        )}
+
+      </div>
+    </div>
+  );
+}
+
+export default Search;
